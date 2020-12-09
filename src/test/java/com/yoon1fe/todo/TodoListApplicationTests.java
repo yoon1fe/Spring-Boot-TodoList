14e8bf2ce0b4cfff9e43e5f17ebb6b870eb34d77
yoon1fe <yoon1fe@naver.com>
2020-12-09 17:45:45 +0900

    spring boot 프로젝트 생성

---
+++ src/test/java/com/yoon1fe/todo/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoon1fe.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
